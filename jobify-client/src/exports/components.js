8d70940c0f8fdd6042f665256ac9b834892fbe87
Mattis Erkensten <m.erkensten@icloud.com>
2022-06-25 23:18:52 +0200

    Sektion 7 klar

--- jobify-client/src/exports/components.js
+++ jobify-client/src/exports/components.js
@@ -1,4 +1,6 @@
 // Denna lösning är nice för att hantera exports och imports av komponenter
 import Logo from '../components/Logo';
+import FormRow from '../components/FormRow';
+import Alert from '../components/Alert';
 
-export { Logo };
+export { Logo, FormRow, Alert };
--- jobify-client/src/pages/Register.jsx
+++ jobify-client/src/pages/Register.jsx
@@ -1,7 +1,73 @@
 import React from 'react';
+import { Logo, FormRow, Alert } from '../exports/components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+  showAlert: false,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = React.useState(initialState);
+  // global state and useNavigate
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  const handleChange = (event) => {
+    console.log(event.target);
+  };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onFormSubmit}>
+        <Logo />
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+        {values.showAlert && <Alert />}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="Name"
+            value={values.name}
+            handleChange={handleChange}
+            labelText="Name"
+          />
+        )}
+
+        <FormRow
+          type="email"
+          name="Email"
+          value={values.email}
+          handleChange={handleChange}
+          labelText="Email"
+        />
+        <FormRow
+          type="password"
+          name="Password"
+          value={values.password}
+          handleChange={handleChange}
+          labelText="Password"
+        />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
