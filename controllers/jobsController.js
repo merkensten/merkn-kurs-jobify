f64fc0737c92dd330a88ddae869b040e2a74c0b1
Mattis Erkensten <m.erkensten@icloud.com>
2022-06-27 14:25:39 +0200

    Sektion 10 klar

---
+++ controllers/jobsController.js
@@ -0,0 +1,17 @@
+const createJob = async (req, res) => {
+  res.send('Create job');
+};
+const deleteJob = async (req, res) => {
+  res.send('delete job');
+};
+const getAllJobs = async (req, res) => {
+  res.send('getAll jobs');
+};
+const updateJob = async (req, res) => {
+  res.send('update job');
+};
+const showStats = async (req, res) => {
+  res.send('Show Stats');
+};
+
+export { createJob, deleteJob, getAllJobs, updateJob, showStats };
--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 // imports
 import express from 'express';
-const app = express();
 import dotenv from 'dotenv';
-dotenv.config();
 
+// db and authenticateUser
 import connectDB from './db/connect.js';
+
+// routers
+import authRouter from './routes/authRouter.js';
+import jobsRouter from './routes/jobsRouter.js';
 
 //middlewares
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 // middlewares
 app.use(notFoundMiddleware);
