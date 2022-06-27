7101ab48e65c96f20fb73f26a0cd127940720e4e
Mattis Erkensten <m.erkensten@icloud.com>
2022-06-27 18:14:46 +0200

    Sektion 13 klar

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,14 +1,27 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (error, req, res, next) => {
-  console.log(error);
+  console.log(error.message);
   const defaultError = {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    message: 'Something went wrong try agian later',
+    statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: error.message || 'Something went wrong try agian later',
   };
-  res
-    .status(defaultError.statusCode)
-    .json({ msg: 'there was an Error', error: error });
+
+  if (error.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.message = Object.values(error.errors)
+      .map((item) => item.message)
+      .join(', ');
+  }
+
+  if (error.code && error.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.message = `${Object.keys(
+      error.keyValue
+    )} field has to be unique`;
+  }
+
+  res.status(defaultError.statusCode).json({ message: defaultError.message });
   next();
 };
 
