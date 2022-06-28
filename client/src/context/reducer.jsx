531c60486d1a3eb3f49f1826cf7f4b5b7f5c8e90
Mattis Erkensten <m.erkensten@icloud.com>
2022-06-29 01:48:51 +0200

    Reducer refaktoriserad till en switch sats

--- client/src/context/reducer.jsx
+++ client/src/context/reducer.jsx
@@ -3,67 +3,60 @@ import { ACTIONS } from '../exports/contexts/actions';
 import { initialState } from '../exports/contexts';
 
 const reducer = (state, action) => {
-  if (action.type === ACTIONS.DISPLAY_ALERT) {
-    return {
-      ...state,
-      showAlert: true,
-      alertType: 'danger',
-      alertText: 'Please provide all values',
-    };
+  switch (action.type) {
+    // handle alerts
+    case ACTIONS.DISPLAY_ALERT:
+      return {
+        ...state,
+        showAlert: true,
+        alertType: 'danger',
+        alertText: 'Please provide all values',
+      };
+    case ACTIONS.CLEAR_ALERT:
+      return {
+        ...state,
+        showAlert: false,
+        alertType: '',
+        alertText: '',
+      };
+    // setup user
+    case ACTIONS.SETUP_USER_BEGIN:
+      return { ...state, isLoading: true };
+    case ACTIONS.SETUP_USER_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        token: action.payload.token,
+        user: action.payload.user,
+        userLocation: action.payload.location,
+        jobLocation: action.payload.location,
+        showAlert: true,
+        alertType: 'success',
+        alertText: action.payload.alertText,
+      };
+    case ACTIONS.SETUP_USER_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        showAlert: true,
+        alertType: 'danger',
+        alertText: action.payload.message,
+      };
+    // toggle sidebar
+    case ACTIONS.TOGGLE_SIDEBAR:
+      return { ...state, showSidebar: !state.showSidebar };
+    // logout user
+    case ACTIONS.LOGOUT_USER:
+      return {
+        ...initialState,
+        user: null,
+        token: null,
+        jobLocation: '',
+        userLocation: '',
+      };
+    default:
+      throw new Error(`no such action: ${action.type}`);
   }
-  if (action.type === ACTIONS.CLEAR_ALERT) {
-    return {
-      ...state,
-      showAlert: false,
-      alertType: '',
-      alertText: '',
-    };
-  }
-
-  // setup User
-  if (action.type === ACTIONS.SETUP_USER_BEGIN) {
-    return { ...state, isLoading: true };
-  }
-  if (action.type === ACTIONS.SETUP_USER_SUCCESS) {
-    return {
-      ...state,
-      isLoading: false,
-      token: action.payload.token,
-      user: action.payload.user,
-      userLocation: action.payload.location,
-      jobLocation: action.payload.location,
-      showAlert: true,
-      alertType: 'success',
-      alertText: action.payload.alertText,
-    };
-  }
-  if (action.type === ACTIONS.SETUP_USER_ERROR) {
-    return {
-      ...state,
-      isLoading: false,
-      showAlert: true,
-      alertType: 'danger',
-      alertText: action.payload.message,
-    };
-  }
-
-  // toggle sidebar
-  if (action.type === ACTIONS.TOGGLE_SIDEBAR) {
-    return { ...state, showSidebar: !state.showSidebar };
-  }
-
-  // Logout user
-  if (action.type === ACTIONS.LOGOUT_USER) {
-    return {
-      ...initialState,
-      user: null,
-      token: null,
-      jobLocation: '',
-      userLocation: '',
-    };
-  }
-
-  throw new Error(`no such action: ${action.type}`);
 };
 
 export { reducer };
