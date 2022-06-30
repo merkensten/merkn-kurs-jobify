f8711cf9a9e291ee3568c5ae62304bb1b4efc1be
Mattis Erkensten <m.erkensten@icloud.com>
2022-06-30 20:51:36 +0200

    Funktionalitet för att ta bort en användare skapad

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -24,6 +24,7 @@ const register = async (req, res) => {
       lastName: user.lastName,
       location: user.location,
       name: user.name,
+      _id: user._id,
     },
     token,
     location: user.location,
@@ -51,11 +52,31 @@ const login = async (req, res) => {
 
   // trick lösning för att dölja lösenordet i user objektet i response
   user.password = undefined;
-  res.status(StatusCodes.OK).json({user, token, location: user.location});
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
   res.send('Login user');
 };
 const updateUser = async (req, res) => {
   res.send('update user');
 };
 
-export { register, login, updateUser };
+// const deleteUser = async (req, res) => {
+//   res.send('delete user');
+// };
+
+const deleteUser = async (req, res) => {
+  try {
+    const response = await User.findByIdAndDelete(req.params.userId);
+    res.status(StatusCodes.OK).send({
+      message: `User with email: ${response.email} deleted successfully!`,
+    });
+  } catch (err) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      message:
+        'Error occured while trying to delete user with id:' +
+        req.params.userId,
+      error: err.message,
+    });
+  }
+};
+
+export { register, login, updateUser, deleteUser };
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { register, login, updateUser } from '../controllers/authController.js';
+import {
+  register,
+  login,
+  updateUser,
+  deleteUser,
+} from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/update-user').patch(updateUser);
+router.route('/delete-user/:userId').delete(deleteUser);
 
 export default router;
